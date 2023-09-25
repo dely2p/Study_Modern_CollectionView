13c6d0067e900da1fab34fe77216f88339a0fab7
dely <lhyal2p@gmail.com>
2023-09-25 14:54:25 +0900

    프로젝트 생성

---
+++ Modern_CollectionView/Modern_CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modern_CollectionView
+//
+//  Created by elly on 2023/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
