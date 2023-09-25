44232f831705a0c19a41a25d02b415662492076a
dely <lhyal2p@gmail.com>
2023-09-25 17:24:09 +0900

    feat: 배너 CollectionView section 구현

--- Modern_CollectionView/Modern_CollectionView/ViewController.swift
+++ Modern_CollectionView/Modern_CollectionView/ViewController.swift
@@ -14,5 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         collectionView.register(BannerCollectionViewCell.self, forCellWithReuseIdentifier: BannerCollectionViewCell.id)
+        collectionView.setCollectionViewLayout(createLayout(), animated: true)
+    }
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        // section index
+        return UICollectionViewCompositionalLayout { [weak self] sectionIndex, _ in
+            return self?.createBannerSection()
+        }
+    }
+    
+    private func createBannerSection() -> NSCollectionLayoutSection {
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        return section
     }
 }
