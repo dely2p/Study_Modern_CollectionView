8d4347fac012aac1035a33dcd157692a47192c1a
dely <lhyal2p@gmail.com>
2023-09-25 15:59:51 +0900

    feat: BannerCollectionViewCell 구현 및 register

---
+++ Modern_CollectionView/Modern_CollectionView/BannerCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  BannerCollectionViewCell.swift
+//  Modern_CollectionView
+//
+//  Created by eunjin on 2023/09/25.
+//
+
+import UIKit
+import SnapKit
+
+class BannerCollectionViewCell: UICollectionViewCell {
+    static let id = "BannerCollectionViewCell"
+    let titleLabel = UILabel()
+    let backgroundImage = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        self.addSubview(titleLabel)
+        self.addSubview(backgroundImage)
+        
+        titleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        backgroundImage.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    public func config(title: String, imageUrl: String) {
+        titleLabel.text = "Title"
+//        imageUrl
+    }
+}
--- Modern_CollectionView/Modern_CollectionView/ViewController.swift
+++ Modern_CollectionView/Modern_CollectionView/ViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.register(BannerCollectionViewCell.self, forCellWithReuseIdentifier: BannerCollectionViewCell.id)
     }
-
-
 }
-
