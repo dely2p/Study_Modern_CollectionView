b06404d881eff7357e5d119b7d3144128c4c15c1
dely <lhyal2p@gmail.com>
2023-09-28 12:48:28 +0900

    feat: TVAndMovie 프로젝트 생성

---
+++ TVAndMovie/TVAndMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVAndMovie
+//
+//  Created by elly on 2023/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
