5ad7fa9540b61eae5d3e02c98aa379d4c8f1b690
dely <lhyal2p@gmail.com>
2023-09-28 14:41:53 +0900

    feat: MainView 레이아웃 및 ButtonView UI 구현

---
+++ TVAndMovie/TVAndMovie/ButtonView.swift
@@ -0,0 +1,50 @@
+//
+//  ButtonView.swift
+//  TVAndMovie
+//
+//  Created by elly on 2023/09/28.
+//
+
+import Foundation
+import UIKit
+
+class ButtonView: UIView {
+    let tvButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("TV", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.configuration = UIButton.Configuration.bordered()
+        return button
+    }()
+    
+    let movieButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Movie", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.configuration = UIButton.Configuration.bordered()
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        self.addSubview(tvButton)
+        self.addSubview(movieButton)
+        
+        tvButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(20)
+        }
+        movieButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(tvButton.snp.trailing).offset(10)
+        }
+    }
+}
--- TVAndMovie/TVAndMovie/ViewController.swift
+++ TVAndMovie/TVAndMovie/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    let buttonView = ButtonView()
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUI()
     }
 
-
+    private func setUI() {
+        self.view.addSubview(buttonView)
+        self.view.addSubview(collectionView)
+        
+        buttonView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+            $0.height.equalTo(80)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(buttonView.snp.bottom)
+        }
+    }
 }
 
